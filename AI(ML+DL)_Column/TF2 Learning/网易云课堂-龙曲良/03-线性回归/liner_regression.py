93197874949fe4d85cf444c4301a8de37403df3e
lihz <lihzworking@163.com>
2022-03-18 16:45:57 +0800

    进度提交

--- AI(ML+DL)_Column/TF2 Learning/网易云课堂-龙曲良/03-线性回归/liner_regression.py
+++ AI(ML+DL)_Column/TF2 Learning/网易云课堂-龙曲良/03-线性回归/liner_regression.py
@@ -1,48 +1,105 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_data(file):
-    """处理数据"""
+    # 从文件导入数据
     data = np.fromfile(file, sep=' ')
-    print(data.shape)
 
-    data1 = np.genfromtxt(file, delimiter=' ')
-    print(data1.shape)
+    # 每条数据包括14项，其中前面13项是影响因素，第14项是相应的房屋价格中位数
+    feature_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE',
+                     'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    feature_num = len(feature_names)
+
+    # 将原始数据进行Reshape，变成[N, 14]这样的形状
+    data = data.reshape([data.shape[0] // feature_num, feature_num])
+
+    # 将原数据集拆分成训练集和测试集
+    # 这里使用80%的数据做训练，20%的数据做测试
+    # 测试集和训练集必须是没有交集的
+    ratio = 0.8
+    offset = int(data.shape[0] * ratio)
+    training_data = data[:offset]
+
+    # 计算训练集的最大值，最小值，平均值
+    maximums, minimums, avgs = training_data.max(axis=0), training_data.min(axis=0), \
+                               training_data.sum(axis=0) / training_data.shape[0]
+
+    # 对数据进行归一化处理
+    for i in range(feature_num):
+        # 归一化公式：x_i = \frac{x_i - min}{max - min}
+        data[:, i] = (data[:, i] - minimums[i]) / (maximums[i] - minimums[i])
+
+    # 训练集和测试集的划分比例
+    training_data = data[:offset]
+    test_data = data[offset:]
+    return training_data, test_data
 
 
-class Model():
-    def __init__(self, trian_x, trian_y):
+class Network(object):
+    def __init__(self, num_of_weights):
         """初始化参数"""
-        self.w = round(random.random(), 3)  # 随机初始化的参数w
-        self.b = round(random.random(), 3)  # 随机初始化的参数b
-        self.partial_w = 0
-        self.partial_b = 0
-        self.lr = 0.0001  # learning rate
-        self.iteration = 100  # 训练的批次
-        self.epoch = 50  # 训练的轮次
-        self.train_x = trian_x
-        self.train_y = trian_y
+        self.w = np.random.randn(num_of_weights, 1)
+        self.b = random.random()
+        self.iteration = 100
+        self.eta = 0.01  # η 学习率
 
-    def relation_x_y(self, x):
-        """预设x、y之间的关系"""
-        hat_y = self.w * x + self.b
-        return hat_y
+    def forward(self, x):
+        """前向传播"""
+        z = np.dot(x, self.w) + self.b
+        return z
 
+    def loss(self, z, y):
+        """计算损失值"""
+        error = z - y
+        num_samples = error.shape[0]
+        cost = error * error
+        cost = np.sum(cost) / num_samples
+        return cost
 
-    def update_param(self):
-        """计算w和b的偏导数，并更新参数"""
-        self.partial_w = np.average(self.train_y - (self.w * self.train_x - self.b) * self.train_x)
-        self.partial_b = np.average(self.train_y - (self.w * self.train_x - self.b))
-        self.w = self.w - self.lr * self.partial_w
-        self.b = self.b - self.lr * self.partial_b
+    def gradient(self, x, y):
+        """计算梯度"""
+        z = self.forward(x)
+        gradient_w = (z - y) * x
+        gradient_w = np.mean(gradient_w, axis=0)
+        gradient_w = gradient_w[:, np.newaxis]
+        gradient_b = (z - y)
+        gradient_b = np.mean(gradient_b)
+        return gradient_w, gradient_b
 
+    def update(self, gradient_w, gradient_b):
+        """更新梯度"""
+        self.w = self.w - self.eta * gradient_w
+        self.b = self.b - self.eta * gradient_b
 
-def main(file):
-    train_x, train_y = load_data(file)
-    model = Model(train_x, train_y)
+    def train(self, x, y, ):
+        """训练模型"""
+        losses = []
+        for i in range(self.iteration):
+            z = self.forward(x)
+            L = self.loss(z, y)
+            gradient_w, gradient_b = self.gradient(x, y)
+            self.update(gradient_w, gradient_b)
+            losses.append(L)
+            if (i + 1) % 10 == 0:
+                print('iter {}, loss {}'.format(i, L))
+        return losses
 
 
 if __name__ == '__main__':
-    data_file = 'housing.data'
-    main(data_file)
+    file = 'housing.data'
+    # 获取数据
+    train_data, test_data = load_data(file)
+    x = train_data[:, :-1]
+    y = train_data[:, -1:]
+    # 创建网络
+    net = Network(x.shape[1])
+    # 启动训练
+    losses = net.train(x, y)
+
+    # 画出损失函数的变化趋势
+    plot_x = np.arange(net.iteration)
+    plot_y = np.array(losses)
+    plt.plot(plot_x, plot_y)
+    plt.show()
